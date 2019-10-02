a351d8e64d43fff13ccc6f5dccdb723c4fbecd94
Raadhesh <35178748+Raadhesh@users.noreply.github.com>
2019-10-03 01:05:12 +0800

    updated
    fixed the errors and bug on the same values b/w RST and partialRST

---
+++ image_retrievalPartialRST.m
@@ -0,0 +1,122 @@
+% The purpose of this program is to do image retrieval from the created
+% database , image folder inside working path
+
+function [prec, rec] = image_retrievalRST(imfile)
+
+tic
+
+working_path = 'D:\Documents\MATLAB\DI&VP\Assignment';
+cd(working_path);
+addpath(pwd);
+
+
+%% Do retrieval for 1 example image
+
+
+%% Step 0: Get query image
+load('final_database_cbir.mat','database');% loading the database mat file
+% prompt = 'Enter image file name , e.g 2.jpg >> ';
+% x = input(prompt,'s') 
+% 
+% x = '88.jpg';
+% imfile = ['.\images\',x];
+
+
+% get feature vector from selected query image
+%HistQ = getColourHistHSV(imfile);
+%HistQ = database(3).featRGB ;
+
+% prompt = 'Enter image file name , e.g 2.jpg >> ';
+% x = input(prompt,'s') ;
+% imfile = ['.\images\',x];
+% prompt = 'Enter image label in database , e.g 1 for africa and 2 for beach >> ';
+% label_true = input(prompt) ;
+    
+
+fprintf('\n\n The query image = %s :', imfile );
+
+figure, imshow(imfile )
+title('Query image');
+featShapePartialRST = getPartialRSTShapeFeatureVec(imfile);
+
+%% Step 1: Find the K most similar images from the datase
+% This is done by comparing the feature vector of the query image and the feature vector of all the image in the database
+%K = 9;% Retrieve the top 9 ,
+numIm = length(database)  ; % this give the number of images in database
+
+for i=1:numIm
+% your code
+v2 = database(i).featShapePartialRST;
+dist(i)=getEuclideanDistance(v2,featShapePartialRST);
+end
+
+%% Step 2: Sort the distance, show most similar image
+% your code
+[value,id] = sort(dist);
+
+%% Step 3
+
+
+%% Step 4, Suppose the user want to show 10 images
+% id_list = [idx(1) idx(2) .....idx(10)] ;
+k=20;
+id_list = id(1:k) ;
+count=0;
+
+
+l = sqrt(k);
+l = round(l);
+b = 0;
+while (b*l)<(k)
+    b = b+1;
+end
+figure('Name', 'Images Retrieved','NumberTitle','off');
+for j=1:k
+   if j==1
+        oriLabel=database(id).label;
+        %continue;
+   end
+    id = id_list(j) ;
+    imfile = database(id_list(j)).imageName ;
+    label = database(id).label; str = sprintf('%d',label);
+    i = sprintf('%s',database(id_list(j)).imageName); 
+    i = erase(i,".\images\");
+    i = erase(i,".gif");
+    str =[ i '   label= ' str] ;
+    subplot(l,b,j) , imshow(imfile) , title(str) ;
+    if label==oriLabel
+        count=count+1;
+    end
+    prec(j) = count/j;
+    rec(j) = count/20;
+end
+
+num_Images = 1:20;
+figure('Name', 'Precision vs Num_images','NumberTitle','off');
+plot(num_Images, prec,'g');
+title('Precision vs Number of Images')
+xlabel('Number of Images')
+ylabel('Precision')
+
+figure('Name', 'Recall Rate vs Num_Images','NumberTitle','off');
+plot(num_Images, rec);
+title('Recall Rate vs Number of Images')
+xlabel('Number of Images')
+ylabel('Recall Rate')
+
+figure('Name', 'Precision vs Recall Rate','NumberTitle','off');
+plot(rec, prec);
+xlabel('Recall Rate')
+ylabel('Precision')
+
+
+
+
+
+
+toc
+
+
+
+
+
--- saveFinalDatabase.m
+++ saveFinalDatabase.m
@@ -12,10 +12,12 @@ load('new2_database_cbir.mat','database');
 databaseNew2 = database;
 load('old_database_cbir.mat','database');
 databaseOld = database;
+clear database;
+database = databaseOld;
 
 for i = 1: length(database)
-    database(i).featShapeRST = databaseNew(i).featShape;
-    database(i).featShapePartialRST = databaseNew2(i).featShape;
+    database(i).featShapeRST = databaseNew(i).featShapeRST;
+    database(i).featShapePartialRST = databaseNew2(i).featShapeRST;
     
     database(i).precisionRST = databaseNew(i).precision;
     database(i).precisionPartialRST = databaseNew2(i).precision;
